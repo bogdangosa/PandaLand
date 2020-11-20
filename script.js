b89559f63d8f0ec89d8e1b80037a24c7d7d10186
bogdangosa <57357871+bogdangosa@users.noreply.github.com>
2020-11-20 17:43:20 +0200

    changed the galery

--- script.js
+++ script.js
@@ -15,13 +15,26 @@ function SetActiveLink(e){
 const whitePandaBtn = document.getElementById("whitePandaBtn");
 const redPandaBtn = document.getElementById("redPandaBtn");
 
+
+const whitePandaImages = document.querySelectorAll(".whiteimg");
+const redPandaImages = document.querySelectorAll(".redimg");
+
 whitePandaBtn.addEventListener("click",ChangeGaleryMode);
 redPandaBtn.addEventListener("click",ChangeGaleryMode);
 
-function ChangeGaleryMode(){
+function ChangeGaleryMode(e){
+    if(e.currentTarget.classList.contains("btn-success")) return;
     whitePandaBtn.classList.toggle("btn-success");
     whitePandaBtn.classList.toggle("btn-light");
 
     redPandaBtn.classList.toggle("btn-success");
     redPandaBtn.classList.toggle("btn-light");
+
+    whitePandaImages.forEach(image=>{
+        image.classList.toggle("hide");
+    });
+    redPandaImages.forEach(image=>{
+        image.classList.toggle("hide");
+    });
+
 }
